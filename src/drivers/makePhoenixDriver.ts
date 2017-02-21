4fab9c62cdea34c15d9a2263c941dbc5639521c2
Faris Mustafa <farismmk@gmail.com>
2017-02-20 23:25:48 -0800

    working phoenix driver

--- src/drivers/makePhoenixDriver.ts
+++ src/drivers/makePhoenixDriver.ts
@@ -1,31 +1,60 @@
-import {Socket} from 'phoenix'
-import xs from 'xstream'
+import {Channel, Socket} from 'phoenix'
+import xs, {Stream} from 'xstream'
 
-interface Options {}
+interface Options {
+  params?: {}
+}
 
-export default function makePhoenixDriver(url: string, opts: Options) {
-  const socket = new Socket(url, opts)
-  socket.connect()
+interface SocketMessage {
+  event: string;
+  payload: {};
+  ref?: string;
+  topic: string;
+}
 
-  return function phoenixDriver(outgoing$) {
-    outgoing$.addListener({
-      next: (payload) => {
-        console.log('sending:', payload)
-        // socket.push(payload)
-      },
-      error: () => {},
-      complete: () => {},
-    })
+class PhoenixSource {
+  private channels: {[key: string]: Channel}
+  private socket: Socket
 
-    return xs.create({
-      start: (listener) => {
-        console.log(socket)
-        socket.onMessage((payload) => {
-          console.log('received:', payload)
-          listener.next(payload)
+  constructor(url: string, opts: Options) {
+    this.channels = {}
+    this.socket = new Socket(url, opts)
+    this.socket.connect()
+  }
+
+  public socket$ = (): Stream<SocketMessage> => {
+    return xs.create<SocketMessage>({
+      start: listener => {
+        this.socket.onMessage(message => {
+          listener.next(message)
         })
       },
       stop: () => {},
     })
+  }
+
+  public channel$ = (topic: string): Stream<SocketMessage> => {
+    if (!this.channels[topic]) {
+      this.channels[topic] = this.socket.channel(topic)
+      this.channels[topic].join()
+    }
+
+    return this.socket$().filter(message => message.topic === topic)
+  }
+
+  public message$ = (topic: string): Stream<SocketMessage> => {
+    return this.channel$(topic)
+      .filter(message => message.event === 'message')
+  }
+
+  public presence$ = (topic: string): Stream<SocketMessage> => {
+    return this.channel$(topic)
+      .filter(message => message.event === 'presence_state' || message.event === 'presence_diff')
+  }
+}
+
+export default function makePhoenixDriver(url: string, opts: Options) {
+  return function phoenixDriver(outgoing$) {
+    return new PhoenixSource(url, opts)
   }
 }
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,27 +1,42 @@
 import {div, input, makeDOMDriver} from '@cycle/dom'
 import {run} from '@cycle/xstream-run'
-import onionify from 'cycle-onionify';
+import onionify from 'cycle-onionify'
 import * as dotenv from 'dotenv'
-import xs from 'xstream'
+import xs, {Stream} from 'xstream'
 dotenv.config()
 
 import UI from './components/ui'
 import makePhoenixDriver from './drivers/makePhoenixDriver'
 
-function intent(DOM) {
-  const updateInputValue$ =
+type Action = {
+  type: string;
+  payload: any;
+}
+
+function intent(DOM): Stream<Action> {
+  const updateInputValueAction$: Stream<Action> =
     DOM.select('.input')
       .events('input')
       .map(ev => ev.target.value)
       .map(payload => ({type: 'updateInputValue', payload}))
 
+  const sendMessageAction$: Stream<Action> =
+    DOM.select('.input')
+      .events('keydown')
+      .filter(ev => {
+        return ev.keyCode === 13
+      })
+      .map(ev => ev.target.value)
+      .map(payload => ({type: 'sendMessage', payload}))
+
   return xs.merge(
-    updateInputValue$,
+    updateInputValueAction$,
+    sendMessageAction$,
   )
 }
 
 function model(action$) {
-  const initialReducer$ = xs.of(function initialReducer(prevState) {
+  const initialReducer$ = xs.of(function initialReducer(state) {
     return {
       inputValue: '',
     }
@@ -29,24 +44,25 @@ function model(action$) {
 
   const updateInputValueReducer$ = action$
     .filter(ac => ac.type === 'updateInputValue')
-    .map(ac => function updateInputValueReducer(prevState) {
+    .map(ac => function updateInputValueReducer(state) {
       return {
-        ...prevState,
+        ...state,
         inputValue: ac.payload,
       }
     })
 
   const sendChatMessageReducer$ = action$
     .filter(ac => ac.type === 'sendMessage')
-    .map(ac => function sendChatMessageReducer(prevState) {
+    .map(ac => function sendChatMessageReducer(state) {
       return {
-        ...prevState,
+        ...state,
       }
     })
 
   return xs.merge(
     initialReducer$,
     updateInputValueReducer$,
+    sendChatMessageReducer$,
   )
 }
 
@@ -54,21 +70,40 @@ function Renderer(sources) {
   const state$ = sources.onion.state$
   const action$ = intent(sources.DOM)
   const reducer$ = model(action$)
+  const outgoingChat$ = action$
+    .filter(ac => ac.type === 'sendMessage')
+    .map(ac => ac.payload)
+  // const incomingChat$ = sources.phoenix
+  //   .startWith('')
+
+  // console.log(sources.phoenix.channel())
+
+  const sock$ = sources
+    .phoenix
+    .socket$()
+    .startWith({})
+
+  const chan$ = sources
+    .phoenix
+    .presence$('world:lobby')
+    .startWith({})
 
   return {
-    DOM: state$.map(state => {
+    onion: reducer$,
+    phoenix: outgoingChat$,
+    DOM: xs.combine(state$, chan$).map(([state, channel]) => {
+      console.log(channel)
       return div([
-        input('.input', {attrs: {type: 'text', value: state.inputValue}}),
+        input('.input', {attrs: {type: 'text'}}),
         UI(sources).DOM,
       ])
     }),
-    onion: reducer$,
   }
 }
 
 const sources = {
   DOM: makeDOMDriver('#app'),
-  phoenix: makePhoenixDriver('ws://localhost:4000/socket', { params: { guardian_token: process.env.JWT } }),
+  phoenix: makePhoenixDriver('ws://localhost:4000/socket', {params: {guardian_token: process.env.JWT}}),
 }
 
 run(onionify(Renderer), sources)
